42bdd5a96a741af2f97695a90cd422abd450bed3
Alejandro Juan Pérez <tuketelamodelmon@gmail.com>
2015-07-16 14:33:23 +0200

    good wavy

--- SDL_ttf_wavy/frag_shad.glsl
+++ SDL_ttf_wavy/frag_shad.glsl
@@ -19,16 +19,17 @@ const float PI = 3.14159265359;
 void main()
 {
 	
-	// take the color of the texture pixel
-	vec4 color1 = texture2D(tex1, gl_TexCoord[0].st);
-	
-	// take another color of the texture pixel
 	vec2 oriTex = gl_TexCoord[0].st;
 	float nt = mod(time, 2*PI);
+	
+	// take the color of the texture pixel
+	vec4 color1 = texture2D( tex2, vec2(oriTex.s+0.005*sin(oriTex.t*PI*4+nt*2), oriTex.t) );
+	
+	// take another color of the texture pixel
 	vec4 color2 = texture2D( tex2, vec2(oriTex.s+0.01*sin(oriTex.t*PI*4+nt), oriTex.t) );
 	
 	// blend both colors
-	float tweak = 1.0f;
+	float tweak = 0.5f;
 	gl_FragColor = mix(color1, color2, tweak);
     
 }
